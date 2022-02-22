909bb09f26e48fc49569aae85287f3058397031f
mahmoud <mahmoud@whipmobility.com>
2022-02-22 15:52:52 +0800

    working on categories screen and theme

---
+++ lib/categories.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals app'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(15),
+        children: DUMMY_CATEGORIES
+            .map((e) => Categoryitem(e.title, e.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
+    );
+  }
+}
---
+++ lib/category_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Categoryitem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  Categoryitem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline3,
+      ),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
